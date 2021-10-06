f00179f01e9dda1307508c01b4499f7a71c8ad64
lotusbleudesign <lotus.b78@gmail.com>
2021-10-06 17:00:28 +0200

    add Query and Mutation

--- index.js
+++ index.js
@@ -8,20 +8,15 @@ const client = new Client({
 client.connect();
 
 const typeDefs = gql`
-
-# Schema User
+  # Schema
+  # Schema User
   type User {
     id: Int
     email: String
     firstName: String
     lastName: String
   }
-  # appel de la requete nommé Query
-  type Query {
-    users: [User]
-  }
-
- # Shema Post
+  # Shema Post
   type MyPost {
     id: Int
     author: String
@@ -30,41 +25,108 @@ const typeDefs = gql`
     updatedAt: String
   }
 
+  # appel de la requete nommé Query
   type Query {
+    users: [User]
     posts: [MyPost]
+    postID: MyPost
   }
 
+  type Mutation {
+    post(
+      author: String
+      content: String
+      comments: String
+      createdAt: String
+    ): MyPost
+
+    delete(id: Int): MyPost
+
+    update(comments: String, updatedAt: String, id: Int): MyPost
+  }
 `;
 
 const resolvers = {
-  // requete SQL
+  // Get
   Query: {
-    users:getUser(),
-    posts: getPost()
+    users: getUser(),
+    posts: getPost(),
+    postID: getPostId(),
+  },
+  // Modification
+  Mutation: {
+    post: addPost,
+    delete: deletePost,
+    update: updatePost,
   },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-
-// TABLE MY USER
-async function getUser() {
-  const { rows } = await client.query("SELECT * FROM myuser");
-  return rows
+// Register user
+async function addUser(firstName, lastName, email) {
+  console.log(`INSERT Job done ${firstName}, ${lastName}, ${email}`);
+  const {
+    rows,
+  } = await client.query(
+    "INSERT INTO myuser (firstName,lastName,email) VALUES ($1,$2,$3) RETURNING *",
+    [firstName, lastName, email]
+  );
+  return rows;
 }
 
-// TABLE MODEL POST
+// Get user
+async function getUser() {
+  const { rows } = await client.query("SELECT * FROM myuser");
+  return rows;
+}
+
+// Read all the posts
 async function getPost() {
-  const {rows} = await client.query("SELECT * FROM model");
-  return rows
+  const { rows } = await client.query("SELECT * FROM model");
+  return rows;
+}
+
+//Create a post
+async function addPost(_, { author, content, comments, createdAt }) {
+  console.log(author, content, comments, createdAt);
+  const {
+    rows,
+  } = await client.query(
+    "INSERT INTO model (author,comments,content,createdAt) VALUES ($1,$2,$3,$4) RETURNING *",
+    [author, content, comments, createdAt]
+  );
+}
+
+// Read a post by It's ID
+async function getPostId(id) {
+  console.log(id);
+  const { rows } = await client.query(
+    "SELECT * from model where (id) = ($1) ",
+    [id]
+  );
+}
+
+async function updatePost(comments, updatedAt, id) {
+  const {
+    rows,
+  } = await client.query(
+    "UPDATE model SET comments=($1), updatedAt=($2) where (id)=($3)",
+    [comments, updatedAt, id]
+  );
+  return rows;
+}
+
+// Delete a post by It's ID
+async function deletePost(id) {
+  const { rows } = await client.query("DELETE FROM model WHERE id=($1)", [id]);
+  return rows;
 }
 
 // --------- Afficher --------
-getUser()
-getPost()
+getUser();
+getPost();
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
-
-
